186eecb6ae8a55e306db389adeb01ea0d2ca5e46
pep <valoy3211@yandex.ru>
2025-09-23 22:47:21 +0500

    Added RawgApiClient and unvaliable API_KEY )))

--- src/main/java/com/game/movie/buddy/Main.java
+++ src/main/java/com/game/movie/buddy/Main.java
@@ -1,10 +1,24 @@
 package com.game.movie.buddy;
 
 
+import model.Game;
+import service.RawgApiClient;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        RawgApiClient rawgApiClient = new RawgApiClient();
 
-
+        try {
+            List<Game> games = rawgApiClient.searchGames("minecraft");
+            for (Game game : games) {
+                System.out.println(game);
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при запросе к API: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
