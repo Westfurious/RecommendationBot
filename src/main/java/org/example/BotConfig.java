184582d181b45a07eb8a7ce959c8fa9348e86636
RealLovee <suvalovdaniil8@gmail.com>
2025-09-28 21:37:27 +0500

    security: remove BotConfig.java with exposed token

--- src/main/java/org/example/BotConfig.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public class BotConfig {
-    public static final String BOT_USERNAME = "oop_practice_game_bot";
-    public static final String BOT_TOKEN = "REVOKED_TOKEN_PLEASE_UPDATE";
-}
